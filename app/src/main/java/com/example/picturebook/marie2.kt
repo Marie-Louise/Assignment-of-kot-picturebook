b86736f6d4a711508a9bce67bc01205292483783
conniewambui <consolatawambuimacharia@gmail.com>
2021-06-13 18:12:03 +0300

    picture

--- app/src/main/java/com/example/picturebook/marie2.kt
+++ app/src/main/java/com/example/picturebook/marie2.kt
@@ -10,10 +10,10 @@ class marie2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_marie2)
-        var image2=findViewById<ImageView>(R.id.imageView2)
-        var text2=findViewById<TextView>(R.id.textView2)
         var next2=findViewById<TextView>(R.id.button2)
         var back2=findViewById<TextView>(R.id.button3)
+        var image2=findViewById<ImageView>(R.id.imageView2)
+        var text2=findViewById<TextView>(R.id.textView2)
         next2.setOnClickListener{
            var intent1= Intent(baseContext,marie3::class.java)
             startActivity(intent1)
--- app/src/main/java/com/example/picturebook/marie4.kt
+++ app/src/main/java/com/example/picturebook/marie4.kt
@@ -10,10 +10,10 @@ class marie4 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_marie4)
-        var image4 = findViewById<ImageView>(R.id.imageView4)
-        var text4 = findViewById<TextView>(R.id.textView3)
         var next4 = findViewById<TextView>(R.id.button6)
         var back4 = findViewById<TextView>(R.id.button7)
+        var image4 = findViewById<ImageView>(R.id.imageView4)
+        var text4 = findViewById<TextView>(R.id.textView3)
         next4.setOnClickListener {
             var intent1 = Intent(baseContext, marie5::class.java)
             startActivity(intent1)
